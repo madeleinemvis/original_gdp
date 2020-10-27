a9b0f4507f157442236c8786cb693640cc05750a
ws1g17 <ws1g17@soton.ac.uk>
2020-10-27 14:20:16 +0000

    Added functions to main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,24 @@ from webretrieval import Crawler, Scraper
 from textprocessing import TextProcessor
 from collections import namedtuple
 
+def get_all_data_from_url(url: str) -> namedtuple:
+    URL_data = namedtuple('URL_data', 'raw_HTML meta_data text_body cleaned_tokens')
+    
+    # scrape the URL
+    scraper = Scraper()
+    initial_html = scraper.scape_url(url)
+
+    # extract the meta data and main body of text from the scraped HTML 
+    processor = TextProcessor()
+    meta_data = processor.extract_meta_data_from_HTML(initial_html)
+    main_text = processor.extract_main_body_from_HTML(initial_html)
+
+    # make the tokens from the main text, and create a clean form
+    tokens = processor.create_tokens_from_text(main_text)
+    cleaned_tokens = processor.clean_tokens(tokens)
+
+    return URL_data(raw_HTML=initial_html, meta_data=meta_data, text_body=main_text, cleaned_tokens=cleaned_tokens)
+
 # Function for the main workflow of the project
 def main():
     NUMBER_OF_KEY_WORDS = 5
@@ -10,28 +28,17 @@ def main():
 
     # Using a dictionary of mapping URL to data for an initial data storage method, will likely need to change
     # very soon
-    URL_data = namedtuple('URL_data', 'raw_HTML meta_data text_body cleaned_tokens')
+    
     scraped_data = {}
         
     # start with the intitial URL
     start_URL = ""
+    scraped_data[start_URL] = get_all_data_from_url(start_URL)  
 
-    # scrape the URL
-    scraper = Scraper()
-    initial_html = scraper.scape_url(start_URL)
-
-    # extract the meta data and main body of text from the scraped HTML 
-    processor = TextProcessor()
-    meta_data = processor.extract_meta_data_from_HTML(initial_html)
-    main_text = processor.extract_main_body_from_HTML(initial_html)
-    urls = processor.extract_urls_from_HTML(initial_html)
-
-    # make the tokens from the main text, and create a clean form
-    tokens = processor.create_tokens_from_text(main_text)
-    cleaned_tokens = processor.clean_tokens(tokens)
-
-    # store all of the inital data
-    scraped_data[start_URL] = URL_data(raw_HTML=initial_html, meta_data=meta_data, text_body=main_text, cleaned_tokens=cleaned_tokens)
+    processor = TextProcessor()  
+    
+    # find all URLs in initial document
+    urls = processor.extract_urls_from_HTML(scraped_data[start_URL].raw_HTML)
     
     # calculate key words from manefesto
     key_words = processor.calculate_key_words(scraped_data[start_URL].cleaned_tokens, NUMBER_OF_KEY_WORDS) 
@@ -45,20 +52,13 @@ def main():
     # do some similarity checking for the documents so far crawled
 
 
-
-
     # recursively crawl the links upto certain depth - includes batch checking so these are the final documents
     final_crawled_urls = crawler.recursive_url_crawl(urls, MAXIMUM_URL_CRAWL_DEPTH)
     urls.extend(final_crawled_urls)
     
-    # this for loop would likely need to be its own method and could be very different, this is just an intial idea
+    # retrieve and store all the data about a URL
     for url in urls:
-        html = scraper.scape_url(url)
-        meta_data = processor.extract_meta_data_from_HTML(html)
-        text = processor.extract_main_body_from_HTML(html)
-        tokens = processor.create_tokens_from_text(text)
-        clean_tokens = processor.clean_tokens(tokens)
-        scraped_data[url] = URL_data(raw_HTML=html, meta_data=meta_data, text_body=text, cleaned_tokens=clean_tokens)
+        scraped_data[url] = get_all_data_from_url(url)
 
     # perform analysis on the scraped data 
 
