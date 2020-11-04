283f7076b6fc2a9e1de972dee087aca8ebd52305
madeleinemvis <mv1g17@soton.ac.uk>
2020-11-04 11:17:22 +0000

    MongoDB API is complete. Added some testing prints to demo its functionality.

--- functions/Main.py
+++ functions/Main.py
@@ -1,15 +1,13 @@
-from analysis import NLP_Analyser
-from webretrieval import Crawler, Scraper
-from textprocessing import TextProcessor
+from datetime import datetime
+
+from functions.webretrieval import Crawler, Scraper
+from functions.textprocessing import TextProcessor
+from functions.API import DbAPI
 from collections import namedtuple
 
 
 def get_all_data_from_url(url: str) -> namedtuple:
-    URL_data = namedtuple('URL_data', 'raw_HTML meta_data text_body cleaned_tokens')
-    
-    # PDFs skipped, should be properly scraped through a PDF scraper
-    if(url.endswith('.pdf')):
-        return
+    URL_data = namedtuple('URL_data', 'raw_HTML text_title, text_body cleaned_tokens')
 
     # scrape the URL
     scraper = Scraper()
@@ -17,14 +15,13 @@ def get_all_data_from_url(url: str) -> namedtuple:
 
     # extract the meta data and main body of text from the scraped HTML 
     processor = TextProcessor()
-    meta_data = processor.extract_meta_data_from_HTML(initial_html)  # to be ignored
-    main_text = processor.extract_main_body_from_html(initial_html)
+    title, main_text = processor.extract_main_body_from_html(initial_html)
 
     # make the tokens from the main text, and create a clean form
     tokens = processor.create_tokens_from_text(main_text)
     cleaned_tokens = processor.clean_tokens(tokens)
 
-    return URL_data(raw_HTML=initial_html, meta_data=meta_data, text_body=main_text, cleaned_tokens=cleaned_tokens)
+    return URL_data(raw_HTML=initial_html, text_title=title, text_body=main_text, cleaned_tokens=cleaned_tokens)
 
 
 # Function for the main workflow of the project
@@ -36,10 +33,14 @@ def main(source_urls: [str]):
 
     processor = TextProcessor()
     crawler = Crawler()
-    analyser = NLP_Analyser()
+    db_api = DbAPI()
 
-    alt_url = "https://www.bbc.co.uk/news/uk-54779430"
-    source_urls.append(alt_url)
+    start_t = datetime.now()
+    documents_queried = db_api.find_documents("WebURLs", "body", "vaccin*")
+    time_taken = datetime.now()-start_t
+    queried_count = db_api.count_documents("WebURLs", "body", "vaccin*")
+
+    print("Time to query: ", time_taken, " for ", queried_count, " documents.")
 
     # Using a dictionary of mapping URL to data for an initial data storage method, will likely need to change
     # very soon
@@ -69,9 +70,10 @@ def main(source_urls: [str]):
     all_tokens = [t for s in scraped_data.values() for t in s.tokens]
     key_words = TextProcessor.calculate_key_words(all_tokens, NUMBER_OF_KEY_WORDS)
 
-    print(f"Sources in manifesto: {len(sources)}")
-    print(f"Sources found in manifesto sources: {len(urls)}")
-    print(f"Top {NUMBER_OF_KEY_WORDS} keywords form manifesto: {key_words}")
+    print("*** KEYWORDS ***")
+    # calculate key words from manifesto
+    key_words = processor.calculate_key_words(scraped_data[start_url].cleaned_tokens,
+                                              NUMBER_OF_KEY_WORDS)  # TODO: Testing of wild cards with Google searching
 
     print("-------- CRAWLING --------")
     # look to crawl with the new data
@@ -89,19 +91,31 @@ def main(source_urls: [str]):
 
     # crawling with Twitter, returns JSON object
     crawled_tweets = crawler.twitter_crawl(key_words, NUMBER_OF_TWEETS_RESULTS_WANTED)
-    for tweet in crawled_tweets:
-        print(tweet)
+    # TODO: Sanitise and Store tweets (TwitterData collection)
+
     # do some similarity checking for the documents so far crawled
 
     # recursively crawl the links upto certain depth - includes batch checking so these are the final documents
     final_crawled_urls = crawler.recursive_url_crawl(urls, MAXIMUM_URL_CRAWL_DEPTH)
     urls.update(final_crawled_urls)
 
-    print("-------- SCRAPING & STORING --------")
-    # retrieve and store all the data about a URL's not yet scraped
-    urls_to_scrape = [u for u in urls if u not in scraped_data.keys()]
-    for url in urls_to_scrape:
-        scraped_data[url] = Scraper.get_data_from_source(url)
+    print("-------- WEB SCRAPING --------")
+    # retrieve and store all the data about a URL
+    urls_list = []
+    for url in urls:
+        start_t = datetime.now()
+        print("Scraping: " + url)
+        scraped_data[url] = get_all_data_from_url(url)
+        # Create dictionary for input
+        url_insert = {"title": getattr(scraped_data[url], "text_title"),
+                      "body": getattr(scraped_data[url], "text_body")
+                      }
+        urls_list.append(url_insert)
+        print("Time Taken: ", (datetime.now()-start_t))
+
+    print("-------- STORING --------")
+    url_col = "WebURLs"
+    db_api.insert_many(url_col, urls_list)
 
     # perform analysis on the scraped data
 
---
+++ functions/webretrieval.py
@@ -0,0 +1,56 @@
+import json
+import requests
+import pandas as pd
+from googlesearch import search
+import requests.exceptions
+import tweepy
+
+
+# class for crawling and scraping the internet
+class Crawler:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def crawl_google_with_key_words(key_words: [str], urls_returned: int) -> [str]:
+        google_result = search(str(key_words), tld="com", lang="en", num=urls_returned, start=0, stop=urls_returned, pause=2.0)
+        return google_result
+
+    # recursively crawl a set of URLs with batch checking similarities
+    @staticmethod
+    def recursive_url_crawl(urls: [str], max_depth: int) -> [str]:
+        return []
+
+    @staticmethod
+    def twitter_init():
+        with open("../twitter_credentials.json", "r") as file:
+            creds = json.load(file)
+
+        auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+        auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+        return api
+
+    def twitter_crawl(self, key_words: [str], tweets_returned: int):
+        api = self.twitter_init()
+        # Retrieves all tweets with given keywords and count
+        tweets = tweepy.Cursor(api.search, q=key_words).items(tweets_returned)
+        # Loops through tweets and retrieves data
+        tweet_list = [[tweet.id, tweet.created_at, tweet.text, tweet.favorite_count, tweet.entities['hashtags'],
+                       tweet.user.location.encode('utf8')] for tweet in tweets]
+        # Stores data into dataframe, may make it easier to store into DB
+        tweets_df = pd.DataFrame(tweet_list, columns=['tweet_id', 'created_at', 'text', 'favorite_count', 'hashtags',
+                                                      'user_location'])
+        return tweets_df.to_json()
+
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    # Alex Ll
+    # method that returns all the HTML data from a URL
+    @staticmethod
+    def scrape_url(url: str) -> str:
+        request = requests.get(url)
+        return request.text
