26ed65d435451ec9a3588f561f39c0e015dcf234
madeleinemvis <mv1g17@soton.ac.uk>
2020-11-05 11:59:06 +0000

    removed Todo lines.

--- functions/Main.py
+++ functions/Main.py
@@ -44,14 +44,17 @@ def main():
 
     print("-------- MANIFESTO --------")
     # start with the initial URL
+<<<<<<< HEAD
     start_url = "https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/"
+=======
+    start_url = "https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/?fbclid=IwAR017eZePLsduO5ZaxM3X8dFkipeQqy58Go8eL3SkuQ4YFtRVSjfBwDMD0A"
+>>>>>>> 470d8fe (removed Todo lines.)
     """ Other URLS:
     - https://vactruth.com/2018/08/30/vaccine-induced-autism/ # faulty (Forbidden with crawler) 
     - https://vactruth.com/2018/05/02/alfie-evans-timeline/
     - https://vactruth.com/2019/06/07/the-vaccination-that-never-should-have-been-approved/
     - https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/
     """
-    # TODO: Testing faulty urls for scraping
 
     scraped_data[start_url] = get_all_data_from_url(start_url)
 
@@ -62,7 +65,7 @@ def main():
     urls = processor.extract_urls_from_html(scraped_data[start_url].raw_HTML)
 
     # calculate key words from manifesto
-    key_words = processor.calculate_key_words(scraped_data[start_url].cleaned_tokens, NUMBER_OF_KEY_WORDS) # TODO: Testing of wild cards with Google searching
+    key_words = processor.calculate_key_words(scraped_data[start_url].cleaned_tokens, NUMBER_OF_KEY_WORDS)
 
     print("-------- CRAWLING --------")
     urls_google = []
