3eff4558a00cb04baf081af0f46a0c37a1a3d360
R_Serpek <rs7g15@soton.ac.uk>
2020-11-09 15:03:53 +0000

    Setting HTTP Request module

--- BackEnd/frontend/src/components/App.js
+++ BackEnd/frontend/src/components/App.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Dashboard from './Dashboard';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 class App extends Component{
 
@@ -33,9 +39,47 @@ class App extends Component{
         
         <React.Fragment>
             
-            <Navbar></Navbar>
-            <br/>
-            {view ? <Home action={this.handler}/> : <Dashboard/>}
+            
+            
+            <Router>
+                <nav className="navbar navbar-expand-sm sticky-top navbar-dark bg-dark">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">Propaganda Analysis</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item active">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                  <Link to="/about">About</Link>
+                                </li> 
+                                <li className="nav-item">
+                                  <Link to="/dashboard" disabled>Dashboard</Link>
+                                </li>                      
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <br/>
+                <Switch>
+                    <Route path="/about">
+
+                    </Route>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route exact path="/">
+                        <Home action={this.handler}/>
+                    </Route>
+                </Switch>  
+            </Router>
+            
+
+            
+            
             
 
         </React.Fragment> 
