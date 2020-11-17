a9c5dc8083d1316b02a9eef2cbf145b98c95e6f7
Alex Lockwood <alex@lockwood.me>
2020-11-17 16:08:23 +0000

    Added ReadabiliPy to scraping

--- functions/Main.py
+++ functions/Main.py
@@ -31,7 +31,8 @@ def main(source_urls: [str]):
         urls.update(data.html_links)
 
     all_tokens = [t for s in scraped_data.values() for t in s.tokens]
-    key_words = TextProcessor.calculate_key_words(all_tokens, NUMBER_OF_KEY_WORDS)
+    all_sentences = " ".join([s.text_body for s in scraped_data.values()])
+    key_words = TextProcessor.calculate_keywords_with_text_rank(all_sentences, NUMBER_OF_KEY_WORDS)
 
     print(f"Sources in manifesto: {len(sources)}")
     print(f"Sources found in manifesto sources: {len(urls)}")
@@ -47,7 +48,7 @@ def main(source_urls: [str]):
     print("-------- SCRAPING --------")
     # retrieve and store all the data about a URL
     for url in urls_google:
-        data = Scraper.get_data_from_source(url)
+        data = scraper.get_data_from_source(url)
         scraped_data[url] = data
         urls.update(data.html_links)
 
--- functions/dataretrieval.py
+++ functions/dataretrieval.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from urllib.error import HTTPError
+
 from functions.textprocessing import TextProcessor
 from tika import parser  # Note this module needs Java to be installed on the system to work.
 from functions.analysis import NLP_Analyser
@@ -10,6 +11,7 @@ import requests.exceptions
 import tweepy
 from urllib.parse import urlparse
 import requests
+from readabilipy import simple_json_from_html_string
 import re
 import csv
 from pathlib import Path
@@ -55,49 +57,44 @@ class Crawler:
             for depth_index in range(0, max_depth):
                 for web_links in url_depth[depth_index]:
                     # Process crawl response
-                    if web_links not in final_list:
-                        response = scraper.scrape_url(web_links)
-                        soup = BeautifulSoup(response, 'html.parser')
-                        tags = soup.find_all('a')
-                    else:
-                        tags = []
+                    data = scraper.get_data_from_source(web_links)
+                    if data is not None:
+                        tags = data.html_links
+                        final_dict[web_links] = data
 
-                    # Loop through web links found in response
-                    for url_new in tags:
-                        # if link empty, continue
-                        if url_new is None:
-                            continue
+                        # Loop through web links found in response
+                        for url_new in tags:
+                            # if link empty, continue
+                            if url_new is None:
+                                continue
 
-                        flag = False  # Flag is true if website has been searched before
-                        parsed_url = urlparse(url_new)
-                        new_link = parsed_url.netloc + parsed_url.path
+                            flag = False  # Flag is true if website has been searched before
+                            parsed_url = urlparse(url_new)
+                            new_link = parsed_url.netloc + parsed_url.path
 
-                        # Check to see if website has been visited before
-                        if new_link in final_dict.keys():
-                            flag = True
-                        else:
-                            for item in url_depth:
-                                parsed_check_urls = [urlparse(x) for x in item]
-                                new_parsed_links = [x.netloc + x.path for x in parsed_check_urls]
-                                if new_link in new_parsed_links:
-                                    flag = True
-                                    break
+                            # Check to see if website has been visited before
+                            if new_link in final_dict.keys():
+                                flag = True
+                            else:
+                                for item in url_depth:
+                                    parsed_check_urls = [urlparse(x) for x in item]
+                                    new_parsed_links = [x.netloc + x.path for x in parsed_check_urls]
+                                    if new_link in new_parsed_links:
+                                        flag = True
+                                        break
 
-                        # If link is not empty and has not been searched before
-                        if url_new is not None and flag is False:
-                            # If link is a valid url
-                            if str(url_new).startswith('http'):
-                                if url_new not in final_list:
-                                    # Append url to search list, will be searched next
-                                    url_depth[depth_index + 1].append(url_new)
+                            # If link is not empty and has not been searched before
+                            if url_new is not None and flag is False:
+                                # If link is a valid url
+                                if str(url_new).startswith('http'):
+                                    # If the link is not blacklisted
+                                    if not re.match(self.BLACKLIST_REGEX, url_new):
+                                        # Append url to search list, will be searched next
+                                        url_depth[depth_index + 1].append(url_new)
 
-                                    # Append to list of valid sites pulled from parent site
-                                    loop.append(url_new)
-
-            # Append loop list to final return list
-            #final_list.append(loop)
-            final_list = final_list + loop
-        return final_list
+                                        # Append to list of valid sites pulled from parent site
+                                        loop.append(url_new)
+        return final_dict
 
     @staticmethod
     def twitter_init():
@@ -173,32 +170,8 @@ Data = namedtuple('Data', 'url raw_html title text_body tokens html_links')
 
 
 class Scraper:
-
-    # Alex Ll
-    # method that returns all the HTML data from a URL
-    @staticmethod
-    def scrape_url(url: str) -> str:
-        try:
-            start_t = datetime.now()
-            request = requests.get(url)
-            print("Scraped: ", url, ". Time taken: ", datetime.now() - start_t)
-        except requests.ConnectionError:
-            print('Connection Error: ' + url)
-            return ''
-        return request.text
-
-    # method to get all of the text out of a pdf, but it does not clean it
-    @staticmethod
-    def scrape_pdf(pdf_path: str) -> str:
-        try:
-            start_t = datetime.now()
-            raw = parser.from_file(pdf_path)
-            raw_text = raw['content']
-            print("Scraped: ", pdf_path, ". Time taken: ", datetime.now() - start_t)
-        except:
-            print('PDF Connection Error: ' + pdf_path)
-            return ''
-        return ' '.join(raw_text.split())
+    def __init__(self):
+        self.processor = TextProcessor()
 
     # method for getting raw text and cleaned tokens from a source, can be a html or '.pdf'
     def get_data_from_source(self, source: str, seen_urls=None) -> namedtuple:
@@ -211,7 +184,10 @@ class Scraper:
                     r")@:%_\+.~#?&//=]*))"
 
         # request the contents of the URL and get the 'content-type' header
-        request = requests.get(source)
+        try:
+            request = requests.get(source)
+        except HTTPError:
+            return None
         content_type = request.headers['content-type']
 
         # if the two MIME types we are looking for aren't present, return None to indicate no data
@@ -223,17 +199,29 @@ class Scraper:
         if "application/pdf" in content_type:
             raw = request.content
             processed_text = parser.from_buffer(raw)['content']
-            main_text = ' '.join(processed_text.split())
-            urls = re.findall(url_regex, main_text)
+            if processed_text is not None:
+                main_text = ' '.join(processed_text.split())
+                urls = re.findall(url_regex, main_text)
+            else:
+                return None
         else:
             if 'location' in request.headers.keys():
                 if source in seen_urls:
                     return None
                 new_seen_urls = seen_urls.append(source)
+                print("Scraped: ", source, ". Time taken: ", datetime.now() - start_t)
                 return self.get_data_from_source(request.headers['location'], new_seen_urls)
-            initial_html = request.text
-            title, main_text = self.processor.extract_main_body_from_html(initial_html)
-            urls = self.processor.extract_urls_from_html(initial_html)
+            initial_html = request.content.decode('utf-8', 'ignore')
+
+            article = simple_json_from_html_string(initial_html, use_readability=True)
+            title = article['title']
+            main_text_unprocessed = article['plain_text']
+            if main_text_unprocessed is None:
+                return None
+            main_text = ''
+            for text in main_text_unprocessed:
+                main_text += text['text'].replace("ï¿1⁄2", "'") + " "
+            urls = self.processor.extract_urls_from_html(article['content'])
         print("Scraped: ", source, ". Time taken: ", datetime.now() - start_t)
 
         # make the tokens from the main text, and create a clean form
@@ -246,4 +234,4 @@ class Scraper:
 
 if __name__ == "__main__":
     scraper = Scraper()
-    print(scraper.get_data_from_source("https://www.pubmedcentral.nih.gov/picrender.fcgi?artid=2480896&blobtype=pdf").html_links)
+    print(scraper.get_data_from_source("https://www.theguardian.com/politics/2020/nov/13/dominic-cummings-has-already-left-job-at-no-10-reports").text_body)
