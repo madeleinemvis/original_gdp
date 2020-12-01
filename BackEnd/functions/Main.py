faa5dc7155fbee1e445c9cb67844c8bcbc9f3fa4
ws1g17 <ws1g17@soton.ac.uk>
2020-12-01 15:00:49 +0000

    Clean up of Main() function by adding generate_manifesto() function General Pep8 cleaning throughout file

--- BackEnd/functions/Main.py
+++ BackEnd/functions/Main.py
@@ -10,6 +10,36 @@ NUMBER_OF_TWEETS_RESULTS_WANTED = 20
 MAXIMUM_URL_CRAWL_DEPTH = 3
 
 
+def generate_manifesto(scraper, text_processor, source_urls, all_sentences, document_html_links, documents):
+    urls = set()
+    scraped_data = {}
+
+    if len(all_sentences) == 0:
+        for source in source_urls:
+            data = scraper.scrape_url(source)
+            scraped_data[source] = data
+            urls.update(data.html_links)
+
+        # if there are less than 5 documents, scrape tokens
+        if len(source_urls) < 5:
+            all_tokens = [t for s in scraped_data.values() for t in s.tokens]
+            key_words_with_scores = TextProcessor.calculate_key_words(all_tokens, NUMBER_OF_KEY_WORDS)
+        else:
+            all_sentences = " ".join([s.text_body for s in scraped_data.values()])
+            key_words_with_scores = text_processor.calculate_keywords_with_text_rank(all_sentences, NUMBER_OF_KEY_WORDS)
+
+        print(f"Sources in manifesto: {len(source_urls)}")
+        print(f"Sources found in manifesto sources: {len(urls)}")
+    else:
+        # If texts stored in database
+        urls.update(document_html_links)
+        key_words_with_scores = text_processor.calculate_keywords_with_text_rank(all_sentences, NUMBER_OF_KEY_WORDS)
+        print(f"Sources in manifesto: {len(documents)}")
+        print(f"Sources found in manifesto sources: {len(document_html_links)}")
+
+    return urls, scraped_data, key_words_with_scores
+
+
 def crawl_google(crawler, key_words):
     # TODO here we are using all 30 keywords for the google search,
     #  we might want to only use 5 or 6 to help with results
@@ -21,7 +51,6 @@ def crawl_google(crawler, key_words):
 
 
 def generate_suggested_urls(no_of_suggestions, google_urls, urls):
-    print("---- GENERATING SUGGESTED URLS FOR USER ----")
     # planning on asking for the approval on the google search results first and then if we want more suggestions,
     # we can add in random ones from the urls seem in the original documents
     if len(google_urls) < no_of_suggestions:
@@ -33,23 +62,10 @@ def generate_suggested_urls(no_of_suggestions, google_urls, urls):
 
 # Function for the main workflow of the project
 def main(source_urls: [str], claim: str):
-
-
-    crawler = Crawler()
+    crawler, scraper, text_processor = Crawler(), Scraper(), TextProcessor()
     db_manager = DbManager()
-    scraper = Scraper()
-    text_processor = TextProcessor()
-    
-    # Using a dictionary of mapping URL to data for an initial data storage method, will likely need to change
-    # very soon
-    scraped_data = {}
 
-    print("-------- MANIFESTO --------")
-
-    # go through each source input and store the main body text, and cleaned tokens
-    # along with the html links found
-    urls = set()
-
+    print("-------- RETRIEVING DATA FROM DB MANAGER --------")
     documents = db_manager.get_all_documents('some_random_hash')
     all_sentences = db_manager.get_all_main_texts('some_random_hash')
     document_html_links = db_manager.get_all_html_links('some_random_hash')
@@ -59,44 +75,19 @@ def main(source_urls: [str], claim: str):
     # TODO: implement - no_of_suggestions = db_manager.get_no_of_suggestions('some_random_hash')
     no_of_suggestions = 25
 
-    # if no tokens stored in database
-    if len(all_sentences) == 0:
-        for source in source_urls:
-            data = scraper.scrape_url(source)
-            scraped_data[source] = data
-            urls.update(data.html_links)
+    print("-------- MANIFESTO --------")
+    urls, scraped_data, key_words_with_scores = generate_manifesto(scraper, text_processor, source_urls, all_sentences,
+                                                                   document_html_links, documents)
 
-        # if there are less than 5 documents, scrape tokens
-        if len(source_urls) < 5:
-            all_tokens = [t for s in scraped_data.values() for t in s.tokens]
-            key_words_with_scores = TextProcessor.calculate_key_words(all_tokens, NUMBER_OF_KEY_WORDS)
-            key_words = [k for k, v in key_words_with_scores]
-            print(f"Most frequent key_words: {key_words}")
-        else:
-            all_sentences = " ".join([s.text_body for s in scraped_data.values()])
-            key_words_with_scores = text_processor.calculate_keywords_with_text_rank(all_sentences, NUMBER_OF_KEY_WORDS)
-            key_words = [word for word, score in key_words_with_scores]
+    key_words = [k for k, v in key_words_with_scores]
+    print(f"Top {NUMBER_OF_KEY_WORDS} keywords from manifesto: {key_words}")
 
-        print(f"Sources in manifesto: {len(source_urls)}")
-        print(f"Sources found in manifesto sources: {len(urls)}")
-    else:
-        # If texts stored in database
-        key_words_with_scores = text_processor.calculate_keywords_with_text_rank(all_sentences, NUMBER_OF_KEY_WORDS)
-        key_words = [word for word, score in key_words_with_scores]
-        urls.update(document_html_links)
-        print(f"Sources in manifesto: {len(documents)}")
-        print(f"Sources found in manifesto sources: {len(document_html_links)}")
-
-    print(f"Top {NUMBER_OF_KEY_WORDS} keywords form manifesto: {key_words}")
-
-    # print("-------- CRAWLING GOOGLE --------")
-    # look to crawl with the new data
-    urls_google = crawl_google(crawler, key_words, NUMBER_OF_GOOGLE_RESULTS_WANTED)
-
+    print("-------- CRAWLING GOOGLE --------")
+    urls_google = crawl_google(crawler, key_words)
 
     if want_suggestions:
+        print("---- GENERATING SUGGESTED URLS FOR USER ----")
         suggestions = generate_suggested_urls(no_of_suggestions, urls_google, urls)
-        print(len(suggestions))
     #
     print("-------- SCRAPING GOOGLE URLS --------")
     # retrieve and store all the data about a URL
@@ -106,20 +97,17 @@ def main(source_urls: [str], claim: str):
             scraped_data[k] = data[k]
             urls.update(data[k].html_links)
 
-
-
-
-
     print("-------- SCRAPING TWITTER --------")
     # crawling with Twitter
-    # crawled_tweets = crawler.twitter_crawl(key_words, NUMBER_OF_TWEETS_RESULTS_WANTED)
+    crawled_tweets = crawler.twitter_crawl(key_words, NUMBER_OF_TWEETS_RESULTS_WANTED)
 
     # print("-------- EXAMPLE SIMILARITY CHECKING --------")
     # do some similarity checking for the documents so far crawled
     # Throws errors if links weren't searched 
-    #analyser.create_topic_model(scraped_data)
-    #print("Similar Doc:", analyser.check_similarity(scraped_data["https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/"]))
-    #print("Non-similar doc:", analyser.check_similarity(scraped_data["https://www.bbc.co.uk/news/uk-54779430"]))
+    # analyser.create_topic_model(scraped_data)
+    # print("Similar Doc:", analyser.check_similarity(scraped_data[
+    # "https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/"]))
+    # print("Non-similar doc:", analyser.check_similarity(scraped_data["https://www.bbc.co.uk/news/uk-54779430"]))
 
     # print("-------- RECURSIVE CRAWLING --------")
     # # recursively crawl the links upto certain depth - includes batch checking so these are the final documents
@@ -145,7 +133,6 @@ def main(source_urls: [str], claim: str):
 
 
 if __name__ == "__main__":
-    # start with the initial URL
     sources = [
         "https://theirishsentinel.com/2020/08/10/depopulation-through-forced-vaccination-the-zero-carbon-solution/",
         "https://www.healthline.com/health/vaccinations/opposition",
