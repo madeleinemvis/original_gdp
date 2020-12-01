a41528a02eec7a67530b8f3028915d2d020d6469
ws1g17 <ws1g17@soton.ac.uk>
2020-12-01 14:29:17 +0000

    Created function for making a list of urls to suggest to the user. Adding in comments of TODO's for post requests Also, moved the google crawling to its own separate function to make main function cleaner

--- BackEnd/functions/Main.py
+++ BackEnd/functions/Main.py
@@ -2,6 +2,29 @@ from BackEnd.functions.dataretrieval import Crawler, Scraper
 from BackEnd.functions.textprocessing import TextProcessor
 from BackEnd.functions.dbmanager import DbManager
 
+import random
+
+
+def crawl_google(crawler, key_words, number_of_results):
+    # TODO here we are using all 30 keywords for the google search,
+    #  we might want to only use 5 or 6 to help with results
+    urls_google = crawler.crawl_google_with_key_words(key_words, number_of_results)
+    print(f"Top {number_of_results} Google Results from Keywords ({key_words}):")
+    for i, url in enumerate(urls_google):
+        print(f"[{i + 1}]: {url}")
+    return urls_google
+
+
+def generate_suggested_urls(no_of_suggestions, google_urls, urls):
+    print("---- GENERATING SUGGESTED URLS FOR USER ----")
+    # planning on asking for the approval on the google search results first and then if we want more suggestions,
+    # we can add in random ones from the urls seem in the original documents
+    if len(google_urls) < no_of_suggestions:
+        suggestions = list(google_urls)
+        return suggestions.extend(random.choices(list(urls), k=no_of_suggestions - len(google_urls)))
+    else:
+        return list(google_urls)[:no_of_suggestions]
+
 
 # Function for the main workflow of the project
 def main(source_urls: [str], claim: str):
@@ -29,6 +52,10 @@ def main(source_urls: [str], claim: str):
     all_sentences = db_manager.get_all_main_texts('some_random_hash')
     document_html_links = db_manager.get_all_html_links('some_random_hash')
     claim = db_manager.get_claim('some_random_hash')
+    # TODO: implement - wantSuggestions = db_manager.get_want_suggestions('some_random_hash')
+    want_suggestions = True
+    # TODO: implement - no_of_suggestions = db_manager.get_no_of_suggestions('some_random_hash')
+    no_of_suggestions = 25
 
     # if no tokens stored in database
     if len(all_sentences) == 0:
@@ -61,23 +88,29 @@ def main(source_urls: [str], claim: str):
     print(f"Top {NUMBER_OF_KEY_WORDS} keywords form manifesto: {key_words}")
 
     # print("-------- CRAWLING GOOGLE --------")
-    # # look to crawl with the new data
-    # urls_google = crawler.crawl_google_with_key_words(key_words, NUMBER_OF_GOOGLE_RESULTS_WANTED)
-    # print(f"Top {NUMBER_OF_GOOGLE_RESULTS_WANTED} Google Results from Keyword {key_words}:")
-    # for i, url in enumerate(urls_google):
-    #     print(f"[{i + 1}]: {url}")
+    # look to crawl with the new data
+    urls_google = crawl_google(crawler, key_words, NUMBER_OF_GOOGLE_RESULTS_WANTED)
+
+
+    if want_suggestions:
+        suggestions = generate_suggested_urls(no_of_suggestions, urls_google, urls)
+        print(len(suggestions))
     #
-    # print("-------- SCRAPING GOOGLE URLS --------")
-    # # retrieve and store all the data about a URL
-    # data = scraper.downloads(urls_google)
-    # if data is not None:
-    #     for k in data.keys():
-    #         scraped_data[k] = data[k]
-    #         urls.update(data[k].html_links)
+    print("-------- SCRAPING GOOGLE URLS --------")
+    # retrieve and store all the data about a URL
+    data = scraper.downloads(urls_google)
+    if data is not None:
+        for k in data.keys():
+            scraped_data[k] = data[k]
+            urls.update(data[k].html_links)
+
+
+
+
 
     print("-------- SCRAPING TWITTER --------")
     # crawling with Twitter
-    crawled_tweets = crawler.twitter_crawl(key_words, NUMBER_OF_TWEETS_RESULTS_WANTED)
+    # crawled_tweets = crawler.twitter_crawl(key_words, NUMBER_OF_TWEETS_RESULTS_WANTED)
 
     # print("-------- EXAMPLE SIMILARITY CHECKING --------")
     # do some similarity checking for the documents so far crawled
@@ -101,7 +134,7 @@ def main(source_urls: [str], claim: str):
     print("-------- STORING --------")
     # db_manager.insert_many('documents_document')  # Collection name for web pages
 
-    db_manager.insert_many('tweets_tweet', crawled_tweets)  # Collection name for tweets
+    # db_manager.insert_many('tweets_tweet', crawled_tweets)  # Collection name for tweets
     # perform analysis on the scraped dataS
 
     # perform data visualisation
