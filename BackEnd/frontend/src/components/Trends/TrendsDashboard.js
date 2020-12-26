07dbafa5522644304e65b024c87e6bcb696998f9
al11g17 <al11g17@soton.ac.uk>
2020-12-26 18:10:46 +0000

    Added explanations to plots

--- BackEnd/frontend/src/components/Trends/TrendsDashboard.js
+++ BackEnd/frontend/src/components/Trends/TrendsDashboard.js
@@ -13,25 +13,31 @@ const TrendsDashboard = props => {
             <Row>
                 <Col className="col-md widget">
                     <EconGauge uid={props.uid}/>
+                    <h6>Plot illustrates how many economic causal tests passed</h6>
                 </Col>
                 <Col className="col-md widget">
                     <EconBar uid={props.uid}/>
+                    <h6>Breakdown of causal estimate and causal refutation values</h6>
                 </Col>
             </Row>
             <Row>
                 <Col className="col-md widget">
                     <HealthGauge uid={props.uid}/>
+                    <h6>Plot illustrates how many health causal tests passed</h6>
                 </Col>
                 <Col className="col-md widget">
                     <HealthBar uid={props.uid}/>
+                    <h6>Breakdown of causal estimate and causal refutation values</h6>
                 </Col>
             </Row>
             <Row>
                 <Col className="col-md widget">
                     <PoliticsGauge uid={props.uid}/>
+                    <h6>Plot illustrates how many politics causal tests passed</h6>
                 </Col>
                 <Col className="col-md widget">
                     <PoliticsBar uid={props.uid}/>
+                    <h6>Breakdown of causal estimate and causal refutation values</h6>
                 </Col>
             </Row>
         </Container>
